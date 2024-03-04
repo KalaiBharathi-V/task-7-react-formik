cff35e79f5be31f32286234f768b0952efe853e2
KalaiBharathi-V <vkalaibharathi@gmail.com>
2024-03-04 18:43:31 +0530

    Formik

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 import './App.css';
 
+const BookSchema = Yup.object().shape({
+  title: Yup.string().required('Title is required'),
+  author: Yup.string().required('Author is required'),
+  isbn: Yup.string().required('ISBN is required'),
+  publicationDate: Yup.date().required('Publication date is required'),
+});
+
 function App() {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.setSubmitting(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <h1>Add Book</h1>
+      <Formik
+        initialValues={{
+          title: '',
+          author: '',
+          isbn: '',
+          publicationDate: '',
+        }}
+        validationSchema={BookSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="title">Title:</label>
+              <Field type="text" name="title" id="title" className="input-field" />
+              <ErrorMessage name="title" component="div" className="error-message" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="author">Author:</label>
+              <Field type="text" name="author" id="author" className="input-field" />
+              <ErrorMessage name="author" component="div" className="error-message" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="isbn">ISBN:</label>
+              <Field type="text" name="isbn" id="isbn" className="input-field" />
+              <ErrorMessage name="isbn" component="div" className="error-message" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="publicationDate">Publication Date:</label>
+              <Field type="date" name="publicationDate" id="publicationDate" className="input-field" />
+              <ErrorMessage name="publicationDate" component="div" className="error-message" />
+            </div>
+            <button type="submit" className="submit-btn" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 }
